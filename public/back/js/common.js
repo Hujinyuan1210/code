a9ebdf9dfcdd4cfa1cc7602a7c3ad24dc80d74a3
Hujinyuan <1126124598@qq.com>
2018-06-25 23:40:53 +0800

    公共功能

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -5,7 +5,7 @@ $(function() {
   
   //一进页面就开始判断用户是否登录
   //先判断当页面是不是登录页面，如果不是在进行判断
-  if(location.href.valueOf('login.html') === -1) {
+  if(location.href.indexOf('login.html') === -1) {
     //判断用户是否登录
 
     $.ajax({
@@ -15,9 +15,9 @@ $(function() {
       success: function (info) {
         console.log(info)
 
-        if (info.error === 400) {
+        if (info.error == 400) {
           //未登录
-          location.href = "./login.index";
+          location.href = "./login.html";
         }
 
       }
@@ -36,4 +36,42 @@ $(function() {
     NProgress.done();
   })
 
+});
+
+//公共功能
+$(function() {
+  //1.左侧二级菜单切换显示
+  $('.lt_aside .category').click(function () {
+    $('.lt_aside .child').stop().slideToggle();
+  })
+
+  //2. 左侧整个侧边栏显示隐藏功能
+  $('.topbar .icon_menu').click(function () {
+    console.log(1111)
+    $('.lt_aside').toggleClass('hidemenu');
+    $('.topbar').toggleClass('hidemenu');
+    $('.lt_main').toggleClass('hidemenu');
+  })
+
+  //3. 点击头部退出按钮, 显示退出模态框
+  $(".icon_logout").click(function () {
+    //显示模态框
+    $("#logoutModal").modal('show');
+  });
+  //4.点击模态框中的退出按钮，需要进行退出操作（ajax）
+  $('#logoutBtn').click(function () {
+    // 发送ajax请求进行退出操作, 让后台销毁当前用户的登陆状态
+    $.ajax({
+      type: 'get',
+      url: '/employee/employeeLogout',
+      success: function (info) {
+        console.log(info);
+        if(info.success) {
+          //跳转到登录页面
+          location.href="login.html";
+        }
+      }
+    })
+  })
+    
 })
